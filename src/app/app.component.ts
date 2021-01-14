01deca8648b053ebb4910484f60e601cd531871b
MartirosSahakyan <martiros.sahakyan@mail.ru>
2021-01-15 00:42:04 +0400

    nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ani-app';
+  showItem = false;
+  showMenu(){
+    if (this.showItem == true) {
+      this.showItem = false;
+    }else{
+      this.showItem = true
+    }
+  }
 }
